b7f287ac0dcc99d312f0022ca4495fcd42c3ca5d
David Wilson <mrdavidjwilson@gmail.com>
2016-05-02 07:22:10 -0700

    Added choreographers to alphaballet soup

--- rohdata/alphaballet_soup.py
+++ rohdata/alphaballet_soup.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import urllib
 import csv
 
+
+#We first try to find the list of performances
+
 data_object_list = []
 
 for i in xrange(0, 368):
@@ -25,13 +28,47 @@ for i in xrange(0, 368):
             data_object['company'] = encoded_data[3]
             data_object_list.append(data_object)
 
+        with open("./data/performance_data.csv", "w") as toWrite:
+            writer = csv.writer(toWrite, delimiter=",")
+            csv_keys = ["date", "mateve", "piece", "company"]
+            writer.writerow(csv_keys)
+            for d_o in data_object_list:
+                writer.writerow([d_o[k] for k in csv_keys])
+
     except IOError:
         print("No internet connection")
 
-with open("./data/data.csv", "w") as toWrite:
-    writer = csv.writer(toWrite, delimiter=",")
-    csv_keys = ["date", "mateve", "piece", "company"]
-    writer.writerow(csv_keys)
-    for d_o in data_object_list:
-        writer.writerow([d_o[k] for k in csv_keys])
+data_object_list = []
+
+for i in xrange(0,20):
+    try:
+        rohurl = "http://www.rohcollections.org.uk/"
+        rohurl += "SearchResults.aspx?searchtype=work"
+        rohurl += "&company=The%20Royal%20Ballet&genre=Ballet&page="
+        rohurl += str(i)
+
+        r = urllib.urlopen(rohurl).read()
+        soup = BeautifulSoup(r)
+
+        pieces = soup.find_all("tr", class_=["odd","even"])
+        for p in pieces:
+            data = p.get_text().split('\n')[1:-1]
+            encoded_data = [d.encode("utf-8") for d in data]
+            components = encoded_data[-1].split(' (')
+            choreographer = components[-1].split(')')[0]
+            piece = ' ('.join(components[:-1])
+            data_object = {}
+            data_object['genre'] = encoded_data[0]
+            data_object['piece'] = piece
+            data_object['choreographer'] = choreographer
+            data_object_list.append(data_object)
+
+        with open("./data/choreographer_data.csv", "w") as toWrite:
+            writer = csv.writer(toWrite, delimiter=",")
+            csv_keys = ["genre", "piece", "choreographer"]
+            writer.writerow(csv_keys)
+            for d_o in data_object_list:
+                writer.writerow([d_o[k] for k in csv_keys])
+    except IOError:
+        print("No internet connection")
 
